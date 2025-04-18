4a6e29d2165deacf51936ff92c2d0302ce6bc067
cjsqudwns <jd8795@naver.com>
2025-04-19 00:35:55 +0900

    feat: Models 추가

--- src/main/java/com/bootcamp/board/BoardApplication.java
+++ src/main/java/com/bootcamp/board/BoardApplication.java
@@ -1,5 +1,6 @@
 package com.bootcamp.board;
 
+import com.bootcamp.board.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
